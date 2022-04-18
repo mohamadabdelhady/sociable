c090cccfd1879efbe7a04d3be7a9674dcd3cce3e
mohamadabdelhady <mohamadhady010@gmail.com>
2022-04-18 17:04:08 +0200

    added chat box

--- resources/js/components/contacts.vue
+++ resources/js/components/contacts.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+    <div class="contact">
         <p class="noti">CONTACTS<span class="result-num">{{contacts.length}}</span></p>
         <div class="card notification-area" v-for="(contact, index) in contacts">
             <div>
@@ -9,9 +10,26 @@
                 <a :href="'get_profile/'+contact.id" class="rm_text_decoration">
                 <span style="vertical-align:bottom" class="ms-2">{{contact.first_name+" "+contact.last_name}}</span>
                 </a>
-                <a href="#"><span class="chat">Chat</span></a>
+                <a href="#" v-on:click="start_chat(contact.id,contact.first_name,contact.last_name,contact.profile_img)"><span class="chat">Chat</span></a>
             </div>
         </div>
+    </div>
+    <div class="chat-contact card">
+        <span style="cursor: pointer; font-size: medium" v-on:click="back"><i style="font-family:Arial, FontAwesome" class="me-1">&#xf060;</i>back</span>
+        <div class="mt-2">
+        <img :src="'storage/'+chat_profile" class="userAvatar" v-if="chat_profile!=null">
+        <img v-else src="images/user_default.svg" class="userAvatar">
+        <a :href="'get_profile/'+chat_id" class="rm_text_decoration">
+            <span style="vertical-align:bottom" class="ms-2">{{chat_name}}</span>
+        </a>
+        </div>
+        <div class="chat-area mt-1">
+
+        </div>
+        <div class="chat-input mt-1">
+            <span><textarea type="text"></textarea><button class="btn ms-1">Chat</button></span>
+        </div>
+    </div>
     </div>
 </template>
 
@@ -23,6 +41,10 @@ export default {
     {
         return{
             contacts:[],
+            chat_name:"",
+            chat_profile:"",
+            chat_id:"",
+            chat:[],
         }
     },
     beforeMount() {
@@ -39,6 +61,26 @@ export default {
                     .catch((error) => {
                         // error.response.status Check status code
                     });
+            },
+            start_chat(id,first_name,last_name,profile)
+            {
+                this.chat_id=id;
+                this.chat_name=first_name+' '+last_name;
+                this.chat_profile=profile;
+                $('.contact').toggle();
+                $('.chat-contact').toggle();
+                axios.get('/get_chat'+id)
+                    .then((res)=>{
+                        this.chat=res.data;
+                    })
+                    .catch((error)=>{
+
+                    });
+            },
+            back()
+            {
+                $('.contact').toggle();
+                $('.chat-contact').toggle();
             }
         }
 }
@@ -50,7 +92,7 @@ export default {
     font-weight: bold;
 }
 .notification-area{
-    height: 200px;
+    height: 50vh;
 }
 .chat{
     padding: 5px;
@@ -60,5 +102,20 @@ export default {
     vertical-align: bottom;
     float: right;
     font-size: .8em;
+}
+.chat-contact{
+    display: none;
+}
+.chat-area{
+    border: 1px solid grey;
+    border-radius: 3px;
+    min-height: 180px;
+}
+.chat-input textarea{
+    border:1px solid grey;
+    outline: none;
+    border-radius: 3px;
+    width: 100%;
+    resize: none;
 }
 </style>
