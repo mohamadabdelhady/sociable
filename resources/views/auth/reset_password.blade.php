7b46c5f313b4b95ef54751b48b516d0f8f8332d7
mohamadabdelhady <mohamadhady010@gmail.com>
2022-09-16 13:16:01 +0000

    fixed the margin and logo img in reset Password page

--- resources/views/auth/reset_password.blade.php
+++ resources/views/auth/reset_password.blade.php
@@ -17,14 +17,12 @@
 </div>
 <div class="container-fluid" style="background-color: #f1f7fc; min-height: 100vh;">
 
-    <div class="login-photo ml-auto mr-auto" style="max-width: 400px !important;">
+    <div class="login-photo ms-auto me-auto" style="max-width: 400px !important;">
         <div class="form-container">
             <form method="POST" action="{{ route('password.update') }}">
                 @csrf
                 <input type="hidden" name="token" value="{{$request->route('token')}}">
-                <div id="logo-img"class="ml-auto mr-auto" ></div>
-
-
+                <div class="navbar-brand ms-auto me-auto" ></div>
                 <div class="form-group">
                     <input type="email" class="form-control @error('email') is-invalid @enderror" id="InputEmail"  placeholder="Email" name="email">
                     @error('email')
@@ -44,7 +42,7 @@
                 <div class="form-group">
                     <input type="password" class="form-control  " id="InputConfirmPassword" placeholder="Confirm password" name="password_confirmation">
                 </div>
-                <div class="form-group"><button class="btn btn-block btn-log" type="submit">Update</button></div>
+                <div class="form-group mt-2"><button class="btn btn-block btn-log" type="submit">Update</button></div>
             </form>
         </div>
     </div>
